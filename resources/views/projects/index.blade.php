c1ec750f6dab062a1cf24f84a3ad7cc026703a35
Evgeny Guguev <evgenygost@gmail.com>
2020-03-04 13:57:38 +0000

    Start frontend width tailwind css

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -6,11 +6,14 @@
         <a href="/projects/create">Create New Project</a>
     </div>
 
-    <ul>
+    <div class="flex">
         @forelse($projects as $project)
-        <li><a href="{{$project->path()}}">{{$project->title}}</a></li>
+            <div class="p-5 bg-white mr-4 rounded shadow w-1/3" style="height: 200px">
+                <h3 class="text-xl mb-4 py-4">{{$project->title}}</h3>
+                <div class="text-gray-600">{{ Str::limit($project->description, 100) }}</div>
+            </div>
         @empty
-            <li>No projects yet</li>
+            <div class="">No projects yet</div>
         @endforelse
-    </ul>
+    </div>
 @endsection
