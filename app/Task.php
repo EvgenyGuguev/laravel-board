1c0b68533ef205a38f3a2bb87cffea2f5357f1b1
Evgeny Guguev <evgenygost@gmail.com>
2020-03-07 13:28:55 +0000

    Some tests and refactoring with activities

--- app/Task.php
+++ app/Task.php
@@ -29,7 +29,11 @@ class Task extends Model
     {
         $this->update(['completed' => true]);
         $this->project->recordActivity('completed_task');
+    }
 
+    public function incomplete()
+    {
+        $this->update(['completed' => false]);
     }
 
     public function project()
