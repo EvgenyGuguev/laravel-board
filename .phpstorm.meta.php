88475da7db775116a64fe2ad07ae214a1b8981e9
Evgeny Guguev <evgenygost@gmail.com>
2020-03-07 12:26:28 +0000

    Start work on prijects activity

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-03-06 13:13:05.
+    * Generated on 2020-03-07 11:42:55.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 6.17.1 on 2020-03-06 13:13:04.
+ * Generated for Laravel 6.17.1 on 2020-03-07 11:42:54.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
