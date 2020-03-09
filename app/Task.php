e905c963435ac6625715646cfb6fc6a425a3b5c6
Evgeny Guguev <evgenygost@gmail.com>
2020-03-09 11:26:23 +0000

    Project Activity cleanup and refactoring

--- app/Task.php
+++ app/Task.php
@@ -15,15 +15,6 @@ class Task extends Model
         'completed' => 'boolean'
     ];
 
-    protected static function boot()
-    {
-        parent::boot();
-
-        static::created(function($task) {
-            $task->project->recordActivity('created_task');
-        });
-
-    }
 
     public function complete()
     {
@@ -34,6 +25,7 @@ class Task extends Model
     public function incomplete()
     {
         $this->update(['completed' => false]);
+        $this->project->recordActivity('incompleted_task');
     }
 
     public function project()
