60aa0d9000440c2a1dcbb97a37d4c2eec2794a61
Evgeny Guguev <evgenygost@gmail.com>
2020-03-10 14:31:04 +0000

    Getting started with Invitations

--- tests/Unit/ProjectTest.php
+++ tests/Unit/ProjectTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -10,21 +11,24 @@ class ProjectTest extends TestCase
 {
     use WithFaker, RefreshDatabase;
 
-    public function test_it_has_a_path()
+    /** @test */
+    public function it_has_a_path()
     {
         $project = factory('App\Project')->create();
 
         $this->assertEquals('/projects/' . $project->id, $project->path());
     }
 
-    public function test_it_belongs_to_an_owner()
+    /** @test */
+    public function it_belongs_to_an_owner()
     {
         $project = factory('App\Project')->create();
 
         $this->assertInstanceOf('App\User', $project->owner);
     }
 
-    public function test_it_can_add_a_task()
+    /** @test */
+    public function it_can_add_a_task()
     {
         $project = factory('App\Project')->create();
 
@@ -32,5 +36,15 @@ class ProjectTest extends TestCase
 
         $this->assertCount(1, $project->tasks);
         $this->assertTrue($project->tasks->contains($task));
+    }
+
+    /** @test */
+    public function it_can_invite_a_user()
+    {
+        $project = factory('App\Project')->create();
+
+        $project->invite($user = factory(User::class)->create());
+
+        $this->assertTrue($project->members->contains($user));
     }
 }
